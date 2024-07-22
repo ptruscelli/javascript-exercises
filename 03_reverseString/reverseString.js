32b65f187af4ee79167f649a96d16b90978b9e1a
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2024-07-22 17:02:42 +0100

    Add completed ex3 and ex4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    //loop starting from end of string, adding each character of original string to new string
+    //so needs to loop from stringlength-1 to 0
+    for (let i = string.length-1;i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+// loop through each element in given array,
+// check if any of the args are present in the array,
+// remove them from array if they are present.
+
+    const newArray = array;
+
+    for (let i = newArray.length-1; i >= 0; i--) {
+        args.forEach((arg) => {
+            if (newArray[i] === arg) {
+                newArray.splice(i, 1);
+            }
+        })
+    }
+    return newArray;
 };
 
 // Do not edit below this line
