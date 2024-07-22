581d98d04f6f690c66d30360db35f182f59a2619
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2024-07-22 18:15:40 +0100

    Add ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let ints = Math.abs(numTwo - numOne);
+    if ((numOne < 0 || numTwo < 0)) {
+            return 'ERROR';
+        } else if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+            return 'ERROR';
+        } else {
+            for (let i = 1; i <= ints + 1; i++) {
+                sum += i;
+            }
+        }
+    
+    return sum;
 };
 
 // Do not edit below this line
