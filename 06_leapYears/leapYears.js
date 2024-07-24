a0b5bdeac2d4cef667fdacfcd96e9ba50247b68d
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2024-07-24 19:01:27 +0100

    Add correct files for ex06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        // divisible by 4 requirement is actually included here
+        // as anything divisible by 400 is also thus divisible by 4
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
