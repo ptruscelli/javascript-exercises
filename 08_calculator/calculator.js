186879cbbca5945870cde77e9d71e237771858d4
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2024-09-07 15:47:08 +0100

    Add ex8 completed files

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((total, num) => (total + num), 0);
+};
+
+const multiply = function(arr) {
+  return arr.reduce((total, num) => (total * num));
+};
+
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(num) {
+  result = num;
 
-const sum = function() {
-	
-};
+	if (num === 1 || num === 0) {
+    return 1;
+  } else {
+    for (let i = num-1; i > 1; i--) {
+      result *= i;
+    }
+  }
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  return result;
 };
 
 // Do not edit below this line
