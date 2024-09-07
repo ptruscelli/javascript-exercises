90a98940cfc2d895ee9ed24eb5696d1d2c6a1318
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2024-09-07 17:00:06 +0100

    Add completed ex9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const newStr = str.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, '').toLowerCase();
+  // remove all whitespace and punctuation, and change to lower case
 
+  const reverse = newStr.split('').reverse().join('');
+  // string into array of characters, reverse it, change back to string
+
+  return newStr == reverse;
 };
 
 // Do not edit below this line
