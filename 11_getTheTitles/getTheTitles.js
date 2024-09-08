9f56b74c0c87fbe38c68730847edcb154d293ab4
ptruscelli <137720740+ptruscelli@users.noreply.github.com>
2024-09-08 17:15:23 +0100

    Add ex11 and ex12 both completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map(book => book.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = (new Date()).getFullYear();
+    sortedArr = arr.sort( (a, b) => {
+        if (a.yearOfDeath == undefined) {
+            lastPerson = currentYear - a.yearOfBirth;
+        } else {
+            lastPerson = a.yearOfDeath - a.yearOfBirth;
+        };
+        if (b.yearOfDeath == undefined) {
+            nextPerson = currentYear - b.yearOfBirth;
+        } else {
+            nextPerson = b.yearOfDeath - b.yearOfBirth;
+        };
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    
+    return sortedArr[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
